2afefe698238c4bd9400e46563cf08a423b6ae22
Toki <tokixsamay@gmail.com>
2026-06-16 21:49:46 +0530

    Create mod.rs to organize instruction modules

---
+++ programs /info_pool/src/instructions/mod.rs
@@ -0,0 +1,15 @@
+pub mod initialize;
+pub mod pyth;
+pub mod threshold;
+pub mod fee;
+pub mod read;
+pub mod governance;
+pub mod pool_metrics;
+
+pub use initialize::*;
+pub use pyth::*;
+pub use threshold::*;
+pub use fee::*;
+pub use read::*;
+pub use governance::*;
+pub use pool_metrics::*;
